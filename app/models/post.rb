6c342795a7e74b33d7883434cb51674dc5727775
ilham bouaik <ibouaik@gmail.com>
2022-08-25 10:57:04 +0100

    add methods and fix offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: :User
   has_many :comments
   has_many :likes
+
+  fter_save :update_counter
+
+  def update_counter
+    author.increment!(:postsCounter)
+  end
+
+  def recent_comments(_post)
+    comments.order(created_at: :desc).limit(5)
+  end
 end
