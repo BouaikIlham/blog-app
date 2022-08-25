ed3cc7913b01de873511eaca8dec23bd12d70bd5
ilham bouaik <ibouaik@gmail.com>
2022-08-25 15:38:08 +0100

    add validations to user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: :User
   has_many :comments
   has_many :likes
+  validates :title, presence: true
+  validates :bio, length: { maximum: 250 }
+  validates :commentsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likescounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   fter_save :update_counter
 
