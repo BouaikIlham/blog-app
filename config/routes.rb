c5fefd38ff08c0d1ef18396d70d29d1b91069386
ilham bouaik <ibouaik@gmail.com>
2022-08-30 12:16:54 +0100

    delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show] 
   end
 
-
-  #  get 'users/:id/posts', to: 'posts#index'
-  #  get 'users/:id/posts/:id', to: 'posts#show'
-
-  #  get 'users/', to: 'users#index'
-  #  get 'users/:id', to: 'users#show'
+  root to: "users#index"
   
 end
