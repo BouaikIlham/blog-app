86073e07d5990b8cb6c64ba0b161535a83e670d5
ilham bouaik <ibouaik@gmail.com>
2022-08-30 22:02:34 +0100

    add test for each action in posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,8 +6,28 @@ RSpec.describe "Posts conroller", :type => :request do
     get "/users/:user_id/posts"
     expect(response).to render_template(:index)
   end
+  it 'return correct response' do
+    get "/users/:user_id/posts"
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'body should includes correct placeholder text' do
+    get "/users/:user_id/posts"
+    expect(response.body).to include('Here is a list of posts for a given user')
+  end
+
+
+
   it "should render a correct template" do
     get "/users/:user_id/posts/:id"
     expect(response).to render_template(:show)
+  end
+  it 'return correct response' do
+    get "/users/:user_id/posts/:id"
+    expect(response).to have_http_status(:ok)
+  end
+  it 'body should includes correct placeholder text' do
+    get "/users/:user_id/posts/:id"
+    expect(response.body).to include('Post content for a given user')
   end
 end
