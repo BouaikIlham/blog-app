0fb6987a4311dca9dde967ead086270c73e39bc1
ilham bouaik <ibouaik@gmail.com>
2022-08-30 21:56:45 +0100

    add tests for each action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Posts conroller", :type => :request do
 
-  it "does  render a posts template" do
+  it "should render a correct template" do
     get "/users/:user_id/posts"
     expect(response).to render_template(:index)
   end
-  it "does  render a posts template" do
+  it "should render a correct template" do
     get "/users/:user_id/posts/:id"
     expect(response).to render_template(:show)
   end
