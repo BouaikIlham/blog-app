7aeb2bda2538350d9f791793db7d4a5e2115c576
ilham bouaik <ibouaik@gmail.com>
2022-09-01 22:50:38 +0100

    add data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+   first_user = User.create(name: 'anne', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+   second_user = User.create(name: 'ariana', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+
+
+  first_post = Post.create(author_id: 32, title: 'Hello', text: 'This is my first post')
+  second_post = Post.create(author_id: 33, title: 'Hello', text: 'Text messages are used for personal, family, business and sociahd')
+  third_post = Post.create(author_id: 32, title: 'Hello', text: 'Text messages are used for personal, family, business and sociahd')
+  fourd_post = Post.create(author_id: 33, title: 'Hello', text: 'Text messages are used for personal, family, business and sociahd')
+  five_post = Post.create(author_id: 32, title: 'Hello', text: 'Text messages are used for personal, family, business and sociahd')
+  six_post = Post.create(author_id: 33, title: 'Hello', text: 'Text messages are used for personal, family, business and sociahd')
+
+
+
+Comment.create(post_id: 56, author_id: 32, text: 'Hi anne!' )
+Comment.create(post_id: 57, author_id: 33, text: 'Hi araina!' )
+Comment.create(post_id: 55, author_id: 32, text: 'Hi anne!' )
+Comment.create(post_id: 53, author_id: 33, text: 'Hi araina!' )
+
+
+Like.create(post_id: 56, author_id: 32)
+Like.create(post_id: 57, author_id: 33)
+Like.create(post_id: 55, author_id: 32)
+Like.create(post_id: 53, author_id: 33)
