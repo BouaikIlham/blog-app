7663ff12204887c4471f3c7a3f5e6008311b189c
ilham bouaik <ibouaik@gmail.com>
2022-09-02 18:35:23 +0100

    create forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
 
 
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show] 
+    resources :users, only: [:index, :show] do 
+     resources :posts, only: [:index, :show,:new, :create] do
+     resources :comments, only: [:create]
+     resources :likes, only: [:create]
+    end
   end
 
   root to: "users#index"
